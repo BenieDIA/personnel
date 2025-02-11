6a0ab14467d99621f846398f3129bb028df6ca8a
bharishh <harish_01@icloud.com>
2025-02-11 14:13:30 +0100

    fichier.sql

--- Personnel/src/sql/bdd.sql
+++ Personnel/src/sql/bdd.sql
@@ -1,19 +1,18 @@
-Create table Employe
-(
-	id integer primary key AUTO_INCREMENT,
-	isAdmin boolean,
-	isSuperAdmin boolean,
-	nom varchar(155),
-	prenom varchar(155),
-	mail varchar(155),
-	password varchar(155),
-	dateArriver date,
-	dateDepart date
+CREATE TABLE Employe (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    isAdmin BOOLEAN NOT NULL,
+    isSuperUser BOOLEAN NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    mail VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    dateArriver DATE NOT NULL,
+    dateDepart DATE,
+    ligue_id INT,
+    FOREIGN KEY (ligue_id) REFERENCES Ligue(id)
 );
 
-Create table Ligue
-(
-	id integer primary key AUTO_INCREMENT,
-	nom varchar(155)
+CREATE TABLE Ligue (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
 );
-
