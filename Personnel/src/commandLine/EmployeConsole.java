432cd0ce89aab552eddb24e3892270f3cd0078f1
bharishh <harish_01@icloud.com>
2025-02-12 17:31:35 +0100

    selectionne employe

--- Personnel/src/commandLine/EmployeConsole.java
+++ Personnel/src/commandLine/EmployeConsole.java
@@ -2,11 +2,18 @@ package commandLine;
 
 import static commandLineMenus.rendering.examples.util.InOut.getString;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 
+import commandLineMenus.List;
 import commandLineMenus.ListOption;
 import commandLineMenus.Menu;
 import commandLineMenus.Option;
 import personnel.Employe;
+import personnel.Ligue;
+import personnel.dateInvalide;
 
 public class EmployeConsole 
 {
@@ -14,24 +21,52 @@ public class EmployeConsole
 	{
 		return new Option("Afficher l'employé", "l", () -> {System.out.println(employe);});
 	}
+	
+	
 
-	ListOption<Employe> editerEmploye()
+	
+	ListOption<Employe> selectEmploye()
 	{
-		return (employe) -> editerEmploye(employe);		
+		return (employe) -> selectEmploye(employe);		
 	}
 
-	Option editerEmploye(Employe employe)
+	Option selectEmploye(Employe employe)
 	{
-			Menu menu = new Menu("Modifier le compte " + employe.getNom(), "c");
+			Menu menu = new Menu("Selectionner " + employe.getNom(), "w");
 			menu.add(afficher(employe));
+			menu.add(Modifier(employe));
+			menu.add(supprimer(employe));
+			menu.addBack("q");
+			return menu;
+			
+			
+	}
+	
+	Option Modifier(Employe employe)
+	{
+			Menu menu = new Menu("Modifier " + employe.getNom(), "m");
 			menu.add(changerNom(employe));
 			menu.add(changerPrenom(employe));
 			menu.add(changerMail(employe));
 			menu.add(changerPassword(employe));
+			menu.add(changerDate(employe));
 			menu.addBack("q");
 			return menu;
-	} 
+
+
+	}
 	
+	private Option supprimer(Employe employe)
+	{
+		return new Option("Supprimer", "v", () -> {try {
+			employe.remove();
+		} catch (dateInvalide e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}});
+	}
+	
+
 
 	private Option changerNom(final Employe employe)
 	{
@@ -55,5 +90,26 @@ public class EmployeConsole
 		return new Option("Changer le password", "x", () -> {employe.setPassword(getString("Nouveau password : "));});
 	}
 	
+ Option changerDate(final Employe employe) throws DateTimeParseException{
+        return new Option("Changer la date d'inscription", "d", () -> {
+            String dateStr = getString("Nouvelle date d'inscription (aaaa-mm-jj) : ");
+            
+            try {
+            	
+            	 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            	    LocalDate date = LocalDate.parse(dateStr, formatter);
+                 employe.setDate(date);
+                 
+            } catch (dateInvalide e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }catch (DateTimeParseException d) {
+                System.out.println("Format de date invalide: " + d.getMessage());
+            }
+           
+        });
+    }
 
 }
+
+
